9fd89fcfed5215e0baa1cae214de7c00142f5cc6
user <user@userdeMacBook-Pro-2.local>
2024-06-11 15:58:17 +0800

    add npm package

---
+++ .prettierrc.cjs
@@ -0,0 +1,11 @@
+
+/* eslint-env node */ module.exports = {
+  $schema: 'https://json.schemastore.org/prettierrc',
+  semi: false,
+  tabWidth: 2,
+  singleQuote: true,
+  printWidth: 100,
+  trailingComma: 'all',
+  plugins: ['prettier-plugin-tailwindcss'],
+  tailwindConfig: './tailwind.config.js',
+}
